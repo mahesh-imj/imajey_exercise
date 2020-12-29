368e87602a15fc23f722fc04e9981645fcd6e601
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-29 11:39:29 +0530

    Update matplotdemo.py

--- matplotdemo.py
+++ matplotdemo.py
@@ -2,10 +2,10 @@ import matplotlib.plot as plt
 from math import *
 for t in range(0,3):
    for x in range(-3,3):
-       y=float((math.e^t)*math.sin(x*t)+(x*x)):
-       plt.plot(x,y, label='for t='t)
-       plt.xlabel('X axis')
-       plt.ylabel('Y axis')
-       plt.title('Graph for equation y=(e^t)*sin(x*t)+(x^2)')
-       plt.legend()
-       plt.show()
+      for y=float((math.e^t)*math.sin(x*t)+(x*x)):
+         plt.plot(x,y, label='for t='t)
+         plt.xlabel('X axis')
+         plt.ylabel('Y axis')
+         plt.title('Graph for equation y=(e^t)*sin(x*t)+(x^2)')
+         plt.legend()
+         plt.show()
