c1e3d364ab5b4fb00638a666b724601820075023
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-29 10:12:43 +0530

    Equation code

--- matplotdemo.py
+++ matplotdemo.py
@@ -2,5 +2,6 @@ import matplotlib.plot as plt
 from math import *
 for t in range(0,3):
    for x in range(-3,3):
-      plt.plot(y=e^t*sin(x*t)+(x*x))
+      y=(math.e^t)*math.sin(x*t)+(x*x)
+      plt.plot(x,y)
       plt.show()
