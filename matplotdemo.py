ba704117dd8de194295dcbf588dda410e666c12e
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-29 10:25:41 +0530

    split into ts

--- matplotdemo.py
+++ matplotdemo.py
@@ -3,8 +3,9 @@ from math import *
 for t in range(0,3):
    for x in range(-3,3):
       for y=(math.e^t)*math.sin(x*t)+(x*x):
-       plt.plot(x,y)
+       plt.plot(x,y, label='for t='t)
        plt.xlabel('X axis')
        plt.ylabel('Y axis')
        plt.title('Graph for equation y=(e^t)*sin(x*t)+(x^2)')
+       plt.legend()
        plt.show()
