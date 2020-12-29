7812c579758da75d26e372fcbd57a2ba7a3fa9f9
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-29 11:55:48 +0530

    f string for t's value in loop

--- matplotdemo.py
+++ matplotdemo.py
@@ -3,7 +3,7 @@ from math import *
 for t in range(0,3):
    for x in range(-3,3):
       for y in float((math.e^t)*math.sin(x*t)+(x*x)):
-         plt.plot(x,y, label='for t='t)
+         plt.plot(x,y, label=f"for t= {t} ")
          plt.xlabel('X axis')
          plt.ylabel('Y axis')
          plt.title('Graph for equation y=(e^t)*sin(x*t)+(x^2)')
