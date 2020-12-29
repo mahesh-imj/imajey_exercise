06caf609d5dfdaa4b6496866aad27e90efb370d1
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-29 11:38:26 +0530

    y answer is in float

--- matplotdemo.py
+++ matplotdemo.py
@@ -2,7 +2,7 @@ import matplotlib.plot as plt
 from math import *
 for t in range(0,3):
    for x in range(-3,3):
-      for y=(math.e^t)*math.sin(x*t)+(x*x):
+       y=float((math.e^t)*math.sin(x*t)+(x*x)):
        plt.plot(x,y, label='for t='t)
        plt.xlabel('X axis')
        plt.ylabel('Y axis')
