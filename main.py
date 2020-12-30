5a0b3dad8a6fb9fae4a11f7e907cf045765d431f
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-30 09:17:11 +0530

    import pip system wheel

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import os
 
 os.system('pip install --upgrade pip')
 os.system('pip install -r requirements')
+os.system('pip install wheel')
+os.system('pip install twine')
+os.system('python example_project/setup.py sdist bdist_wheel')
+os.system('twine upload dist/*')
 os.system('pip install --upgrade matplotlib')
