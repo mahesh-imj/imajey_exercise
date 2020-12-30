bd0abc051c07064685761920ce5108de2f98dedb
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-30 09:42:03 +0530

    indent and math

--- matplotdemo.py
+++ matplotdemo.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt
-%matplotlib inline
-from math import *
+import math
 x=[i for i in range(-3,3)]
 for t in range(0,3):
-   y=[float((math.e^t)*math.sin(i*t)+(i*i)) for i in x]  
-         plt.plot(x,y, label=f"for t= {t} ")
-         plt.xlabel('X axis')
-         plt.ylabel('Y axis')
-         plt.title('Graph for equation y=(e^t)*sin(x*t)+(x^2)')
-         plt.legend()
-         plt.show()
+  y=[float((math.e^t)*math.sin(i*t)+(i*i)) for i in x]  
+  plt.plot(x,y,label=f"for t= {t} ")
+  plt.xlabel('X axis')
+  plt.ylabel('Y axis')
+  plt.title('Graph for equation y=(e^t)*sin(x*t)+(x^2)')
+  plt.legend()
+  plt.show()
