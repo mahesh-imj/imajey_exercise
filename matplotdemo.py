609fdfb8b7659e1ec4582ac94cffd09818ee44d2
mahesh-imj <76428515+mahesh-imj@users.noreply.github.com>
2020-12-31 09:44:19 +0530

    Shows offline for t=many

--- matplotdemo.py
+++ matplotdemo.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 import math
-x=[i for i in range(-3,3)]
-for t in range(0,3):
-  y=[float((math.e^t)*math.sin(i*t)+(i*i)) for i in x]  
+for t in range(0,5):
+  x=[i for i in range(-3,4)]
+  #using map function with lambda may help  
+  y=[float((math.e**t)*math.sin(i*t)+(i**2)) for i in x]
   plt.plot(x,y,label=f"for t= {t} ")
   plt.xlabel('X axis')
   plt.ylabel('Y axis')
